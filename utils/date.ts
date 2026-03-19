2d6130d713435df2f6a4f17025149de9ebc88c6b
Muhammad Luthfi Khusyasy <44360105+Khusyasy@users.noreply.github.com>
2026-03-19 13:53:34 +0700

    fix: utc offset flickering

--- utils/date.ts
+++ utils/date.ts
@@ -2,10 +2,10 @@ import dayjs from 'dayjs'
 import relativeTime from 'dayjs/plugin/relativeTime'
 import utc from 'dayjs/plugin/utc'
 
-export const UTC_OFFSETS = ref([
-  -14, -13, -12, -11, -10, -9, -8, -7, -6, -5, -4, -3, -2, -1,
-  0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12,
-])
+export const UTC_OFFSETS = [
+  -14, -13, -12, -11, -10, -9, -8, -7, -6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5,
+  6, 7, 8, 9, 10, 11, 12,
+]
 
 dayjs.extend(relativeTime)
 dayjs.extend(utc)
@@ -26,7 +26,12 @@ export const timeFormat = (date: Date | null) => {
 }
 
 export const offsetFormat = (offset: number) => {
-  return dayjs().utc().utcOffset(offset).hour(0).local().format('HH:00');
+  return dayjs('2003-11-05') // fixed date just to get offset
+    .utc()
+    .utcOffset(offset)
+    .hour(0)
+    .local()
+    .format('HH:00')
 }
 
 // TODO: probably need to refactor so it doesnt return dayjs directly
